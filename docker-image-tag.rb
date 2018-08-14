819bd4c8e91fe6193ded4d3ea12e445c5c38fa58
stefanwalther <stefan@walthers.me>
2018-08-14 01:37:02 -0400

    Brew formula update for docker-image-tag version v0.1.2

--- docker-image-tag.rb
+++ docker-image-tag.rb
@@ -1,9 +1,9 @@
 class DockerImageTag < Formula
   desc "CLI tool to search for tags in Docker images (using Docker Registry v2)."
   homepage "https://github.com/stefanwalther/"
-  url "https://github.com/stefanwalther/docker-image-tag/releases/download/v0.1.1-brew-2/docker-image-tag_0.1.1-brew-2_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.1.1-brew-2"
-  sha256 "bfcb799afab00c111fe004540eea5ed197271e803f86ab31a281a722ae19eb56"
+  url "https://github.com/stefanwalther/docker-image-tag/releases/download/v0.1.2/docker-image-tag_0.1.2_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.1.2"
+  sha256 "28b8b7b968f8b42e4f99a3619a9b62f1e8432e0d23b121ca8aef08f1d3243941"
 
   def install
     bin.install "docker-image-tag"
